857d1f7dd38141f38eef4155bc923268a6d5280b
Mick Zhang <39885425+mick-zhang@users.noreply.github.com>
2019-09-27 20:02:19 -0400

    Update 3.4_Deque.py

--- 3.4_Deque.py
+++ 3.4_Deque.py
@@ -19,3 +19,16 @@ class Deque:
     
     def size(self):
         return len(self.items)
+
+d = Deque()
+
+print(d.isEmpty())
+d.addRear(4)
+d.addRear("dog")
+d.addFront("cat")
+d.addFront(True)
+print(d.size())
+print(d.isEmpty())
+d.addRear(8.4)
+print(d.removeRear())
+print(d.removeFront())
